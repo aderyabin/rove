c555997796d76e3c1a4ca032c618aca8d549817c
Andrey Deryabin <deriabin@gmail.com>
2013-04-03 08:57:02 +0400

    fix ruby package

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -1,8 +1,7 @@
 Hospice.package 'Ruby' => 'Languages' do
   select 'Ruby Manager' do
     option 'rvm' do
-      cookbook 'rbenv', :github => 'fnichol/chef-rbenv'
-      cookbook 'ruby_build', :github => 'fnichol/chef-ruby_build', :ref => 'v0.7.2'
+      cookbook 'rbenv', :github => 'fnichol/chef-rvm', :ref => 'v0.9.0'
       recipe 'rbenv::user'
       recipe 'ruby_build'
     end
@@ -21,6 +20,21 @@ Hospice.package 'Ruby' => 'Languages' do
                 {
                   :user => 'vagrant',
                   :rubies => ['1.8.7']
+                }
+              ]
+            }
+          }
+        end
+      end
+
+      option '2.0.0' do
+        config do
+          {
+            :rbenv => {
+              :user_installs => [
+                {
+                  :user => 'vagrant',
+                  :rubies => ['2.0.0p0']
                 }
               ]
             }
