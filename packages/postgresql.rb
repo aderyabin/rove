936e96bd4c6a1b7a7530a9717ff68006c3fdfc3c
Andrey Deryabin <deriabin@gmail.com>
2013-04-11 18:31:23 +0400

    update pg package

--- packages/postgresql.rb
+++ packages/postgresql.rb
@@ -2,6 +2,22 @@ Hospice.package :postgresql do
   title 'PostgreSQL'
   category 'Databases'
 
-  cookbook 'postgresql', :github => 'express42-cookbooks/postgresql'
-  recipe 'postgresql'
+  cookbook 'postgresql'
+  recipe 'postgresql::server'
+
+  config do |config|
+    config["postgresql"] = {
+      :config => {
+        :listen_addresses => '*'
+      },
+      "password" => {
+        "postgres" => "password"
+      },
+      :pg_hba => [
+        {:type => 'local', :db => 'postgres', :user => 'postgres', :addr => nil, :method => 'trust'},
+        {:type => 'host', :db => 'all', :user => 'all', :addr => '0.0.0.0/0', :method => 'md5'},
+        {:type => 'host', :db => 'all', :user => 'all', :addr => '::1/0', :method => 'md5'}
+      ]
+    }
+  end
 end
