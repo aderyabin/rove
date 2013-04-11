5c0688295e9574d024e436e6eb1894a593dd450d
Andrey Deryabin <deriabin@gmail.com>
2013-04-11 18:31:11 +0400

    update mysql package

--- packages/mysql.rb
+++ packages/mysql.rb
@@ -3,5 +3,12 @@ Hospice.package :mysql do
   category 'Databases'
 
   cookbook 'mysql'
-  recipe 'mysql'
+  recipe 'mysql::server'
+  config do |config|
+    config["mysql"] = {
+      "server_root_password" => "password",
+      "server_repl_password" => "password",
+      "server_debian_password" => "password"
+    }
+  end
 end
