7c367d3576a65422d08e384dc93cacc02f6450a4
Andrey Deryabin <deriabin@gmail.com>
2013-04-12 11:34:01 +0400

    inputs for web-servers

--- packages/apache.rb
+++ packages/apache.rb
@@ -1,6 +1,80 @@
 Hospice.package :apache do
   category 'Web-Servers'
-
   cookbook 'apache2'
   recipe 'apache2'
+
+  input :dir do
+    default '/etc/apache2'
+    config {|value| {apache: {dir: value}}}
+  end
+
+  input :log_dir do
+    default '/var/log/apache2'
+    config {|value| {apache: {log_dir: value}}}
+  end
+
+  input :error_log do
+    default 'error.log'
+    config {|value| {apache: {error_log: value}}}
+  end
+
+  input :user do
+    default 'www-data'
+    config {|value| {apache: {user: value}}}
+  end
+
+  input :group do
+    default 'www-data'
+    config {|value| {apache: {group: value}}}
+  end
+
+  input :binary do
+    default '/usr/sbin/apache2'
+    config {|value| {apache: {binary: value}}}
+  end
+
+  input :cache_dir do
+    default '/var/cache/apache2'
+    config {|value| {apache: {cache_dir: value}}}
+  end
+
+  input :pid_file do
+    default '/var/run/apache2.pid'
+    config {|value| {apache: {pid_file: value}}}
+  end
+
+  input :lib_dir do
+    default '/usr/lib/apache2'
+    config {|value| {apache: {lib_dir: value}}}
+  end
+
+  input :listen_ports do
+    default '80'
+    config {|value| {apache: {listen_ports: [value]}}}
+  end
+
+  input :contact do
+    default 'ops@example.com'
+    config {|value| {apache: {contact: value}}}
+  end
+
+  input :timeout do
+    default '300'
+    config {|value| {apache: {timeout: value}}}
+  end
+
+  input :keepalive do
+    default 'On'
+    config {|value| {apache: {keepalive: value}}}
+  end
+
+  input :keepaliverequests do
+    default '100'
+    config {|value| {apache: {keepaliverequests: value}}}
+  end
+
+  input :keepalivetimeout do
+    default '5'
+    config {|value| {apache: {keepalivetimeout: value}}}
+  end
 end
--- packages/nginx.rb
+++ packages/nginx.rb
@@ -1,6 +1,45 @@
 Hospice.package :nginx do
   category 'Web-Servers'
-
-  cookbook 'nginx', :github => 'opscode-cookbooks/nginx'
+  cookbook 'nginx'
   recipe 'nginx'
+
+  input :version do
+    default '1.2.6'
+    config {|value| {nginx: {version: value}}}
+  end
+
+  input :dir do
+    default '/etc/nginx'
+    config {|value| {nginx: {dir: value}}}
+  end
+
+  input :log_dir do
+    default '/var/log/nginx'
+    config {|value| {nginx: {log_dir: value}}}
+  end
+
+  input :binary do
+    default '/usr/sbin/nginx'
+    config {|value| {nginx: {binary: value}}}
+  end
+
+  input :user do
+    default 'www-data'
+    config {|value| {nginx: {user: value}}}
+  end
+
+  input :init_style do
+    default 'runit'
+    config {|value| {nginx: {init_style: value}}}
+  end
+
+  input :pid do
+    default '/var/run/nginx.pid'
+    config {|value| {nginx: {pid: value}}}
+  end
+
+  input :worker_connections do
+    default '1024'
+    config {|value| {nginx: {worker_connections: value}}}
+  end
 end
