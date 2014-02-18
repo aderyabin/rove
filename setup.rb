424e5096cbd37bfcca3e791118b44a5015a11390
Rob Murray <robmurray17@gmail.com>
2014-02-18 20:51:37 +0000

    Load vagrant settings dir

--- setup.rb
+++ setup.rb
@@ -2,3 +2,4 @@ require_relative 'lib/rove'
 
 Rove.load! "#{File.dirname(__FILE__)}/packages"
 Rove.load! "#{File.dirname(__FILE__)}/patterns"
+Rove.load! "#{File.dirname(__FILE__)}/vagrant_settings"
